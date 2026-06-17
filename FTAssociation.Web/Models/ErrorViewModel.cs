647965a401d87245f640296938e677ef1c97ac3b
Gayathri-prog <gayathrimudduluru24@gmail.com>
2026-06-17 15:29:09 +0530

    Initial project commit

---
+++ FTAssociation.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FTAssociation.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
